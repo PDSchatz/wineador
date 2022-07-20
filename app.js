d627d22b3a5d4e93577c237f1ad7f324bcb780ef
gucci <pdschatz@gmail.com>
2022-07-20 17:02:10 -0400

    was in wrong director during last commit -

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const Express = require('express'),
       cors = require('cors'),
       mongoose = require('mongoose'),
       app = Express(),
+
       PORT = process.env.PORT || 4000,
       HOST = process.env.HOST || "127.0.0.1",
-      db = require("./db")
+
+      db = require("./db"),
+      authMiddleware = require('./controllers/auth')
 
 app.use(cors())
 app.use(Express.json())
+app.use("/api/auth/", authMiddleware)
 
 app.listen(PORT, HOST, () => {
   try {
